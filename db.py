fbbb8e56ed01dd59767b1bc7acbb898ec4ee31d8
Arindom41 <arindomp1441@gmail.com>
2026-06-10 02:50:40 +0530

    Fix Neon connection pooling

--- db.py
+++ db.py
@@ -4,7 +4,11 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300
+)
 Session = sessionmaker(bind=engine)
 
 session = Session()
